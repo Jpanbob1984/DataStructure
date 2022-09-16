b9a4b01bcc4ed36e7fc60e0917e8251ec7f760ac
link <dc1152774133@gmail.com>
2022-09-16 23:34:29 +0800

    1

--- src/main/java/abc_001.java
+++ src/main/java/abc_001.java
@@ -5,10 +5,10 @@ public class abc_001 {
     //如果a+b+c=1000，且a^2+b^2=c^2,如何求出所有a,b,c可能的组合
     public static void main(String[] args) {
         Date d1 = new Date();
-        int m = 1000;
-//        for (int a = 0; a <= m; a++) {
-//            for (int b = 0; b <= m; b++) {
-//                for (int c = 0; c <= m; c++) {
+        int n = 1000;
+//        for (int a = 0; a <= n; a++) {
+//            for (int b = 0; b <= n; b++) {
+//                for (int c = 0; c <= n; c++) {
 //                     if (a+b+c==1000 && a*a+b*b==c*c){
 //                         System.out.println(a+","+b+","+c);
 //                     }
@@ -23,14 +23,15 @@ public class abc_001 {
 //        T = N * N * 2
 //        抽象成函数为  T(n) = n^3 * 2
 
-        for (int a = 0; a <= m; a++) {
-            for (int b = 0; b <= m; b++) {
-                int c = m - a - b;
+        for (int a = 0; a <= n; a++) {
+            for (int b = 0; b <= n; b++) {
+                int c = n - a - b;
                 if (a * a + b * b == c * c) {
                     System.out.println(a + "," + b + "," + c);
                 }
             }
         }
+        //T(n) =
 
         Date d2 = new Date();
         System.out.println((d2.getTime() - d1.getTime()) + "毫秒");
